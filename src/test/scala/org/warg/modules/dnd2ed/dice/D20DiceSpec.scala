94f4932580659ab3f2a8a6a4faeaf80dc902992e
Stanislavs Rubens <stanislavs.rubens@leongaming.com>
2014-06-03 09:58:36 +0200

    adjusted incorrect test

--- src/test/scala/org/warg/modules/dnd2ed/dice/D20DiceSpec.scala
+++ src/test/scala/org/warg/modules/dnd2ed/dice/D20DiceSpec.scala
@@ -8,8 +8,8 @@ class D20DiceSpec extends FlatSpec {
 
   it should "roll non-zero values from 1 to 20 inclusive only" in {
 
-    for (i <- 1 to 1000) {
-      var roll = D6Dice.roll()
+    for (i <- 1 to 10000) {
+      var roll = D20Dice.roll()
       assert(roll != 0 && roll >= 1 && roll <= 20)
     }
   }
